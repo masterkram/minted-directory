304108be6d90e0c72b10e0fa57bdf47ce14f3469
Mark Bruderer <markbruderer123@gmail.com>
2024-07-06 17:03:13 +0200

    fix: dark mode submit.

--- components/SubmitBox.vue
+++ components/SubmitBox.vue
@@ -8,8 +8,8 @@ const hidden: Ref<boolean> = useCookie('hide-submit-box');
 
 <template>
   <div v-if="!hidden"
-    class="border bg-secondary-50 border-secondary-200 dark:border-gray-500 hover:border-solid rounded p-6 relative group transition-all">
-    <p class="font-bold">{{ config.submit.title }}</p>
+    class="border bg-secondary-50 dark:bg-secondary-400/20 border-secondary-200 dark:border-secondary-400 hover:border-solid rounded p-6 relative group transition-all">
+    <p class="font-bold dark:text-white">{{ config.submit.title }}</p>
     <p>{{ config.submit.description }}</p>
     <div class="flex gap-2 mt-3">
       <Button icon="ph:rocket">Submit</Button>
