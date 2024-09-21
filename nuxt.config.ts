4a09af6aaec5c0c19c7d533efb2b7c8b6cdbb2b3
Mark Bruderer <markbruderer123@gmail.com>
2024-09-21 17:12:43 +0200

    chore: rename nuxt icon.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
     "@nuxtjs/tailwindcss",
     "@nuxt/content",
     "@nuxtjs/google-fonts",
-    "nuxt-icon",
+    "@nuxt/icon",
     "@nuxthq/studio",
     "@nuxtjs/color-mode",
     "@nuxtjs/seo",
