6d7aa7615771512404e946c309bffffce9b9cecd
Mark Bruderer <markbruderer123@gmail.com>
2024-09-29 00:37:26 +0200

    fix: show bullet and card at same time.

--- components/ui/Card.vue
+++ components/ui/Card.vue
@@ -5,35 +5,35 @@ defineProps(['item']);
 </script>
 
 <template>
-  <div>
-    <NuxtLink :to="item._path" v-if="cardConfig?.type !== 'bullet'"
-      class="border col-span-1 border-gray-200 dark:border-gray-500 hover:border-primary-400 hover:border-solid dark:hover:border-primary-300 rounded relative group transition-all"
-      :class="cardConfig?.type === 'shadow' ? 'shadow-sm' : cardConfig?.type === 'dashed' ? 'border-dashed' : ''">
-      <DirectoryFeaturedTag class="ml-6" v-if="item.featured" />
-      <NuxtImg v-if="item.card_image" sizes="400px" class="w-full h-48 rounded-t object-cover"
-        :src="item.card_image ?? '/logo.png'" />
-      <div v-else
-        class="w-full h-48 flex justify-center items-center rounded-t font-bold text-2xl bg-gray-200 dark:bg-gray-600">
+
+  <NuxtLink :to="item._path" v-if="cardConfig?.type !== 'bullet'"
+    class="border col-span-1 border-gray-200 dark:border-gray-500 hover:border-primary-400 hover:border-solid dark:hover:border-primary-300 rounded relative group transition-all"
+    :class="cardConfig?.type === 'shadow' ? 'shadow-sm' : cardConfig?.type === 'dashed' ? 'border-dashed' : ''">
+    <DirectoryFeaturedTag class="ml-6" v-if="item.featured" />
+    <NuxtImg v-if="item.card_image" sizes="400px" class="w-full h-48 rounded-t object-cover"
+      :src="item.card_image ?? '/logo.png'" />
+    <div v-else
+      class="w-full h-48 flex justify-center items-center rounded-t font-bold text-2xl bg-gray-200 dark:bg-gray-600">
+      {{ item.title }}
+    </div>
+    <div class="p-6">
+      <h3 class="m-0 text-lg font-semibold dark:text-gray-50">
         {{ item.title }}
+      </h3>
+      <p>
+        {{ item.description }}
+      </p>
+      <div class="p-0 mt-2 flex gap-2">
+        <UiTag v-for="tag in item.tags" :tag="tag" />
       </div>
-      <div class="p-6">
-        <h3 class="m-0 text-lg font-semibold dark:text-gray-50">
-          {{ item.title }}
-        </h3>
-        <p>
-          {{ item.description }}
-        </p>
-        <div class="p-0 mt-2 flex gap-2">
-          <UiTag v-for="tag in item.tags" :tag="tag" />
-        </div>
-      </div>
-    </NuxtLink>
-    <NuxtLink :to="item._path">
-      <div class="inline-flex gap-2 items-center">
-        <img class="h-5 w-5" :src="item.cover ?? '/logo.png'" />
-        <span class="font-semibold dark:text-gray-50">{{ item.title }}</span> -
-        <span class="text-gray-600 dark:text-gray-300">{{ item.description }}</span>
-      </div>
-    </NuxtLink>
-  </div>
+    </div>
+  </NuxtLink>
+  <NuxtLink v-else :to="item._path">
+    <div class="inline-flex gap-2 items-center">
+      <img class="h-5 w-5" :src="item.cover ?? '/logo.png'" />
+      <span class="font-semibold dark:text-gray-50">{{ item.title }}</span> -
+      <span class="text-gray-600 dark:text-gray-300">{{ item.description }}</span>
+    </div>
+  </NuxtLink>
+
 </template>
