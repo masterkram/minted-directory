f700c2694b38a6dca82bf50a3dc0d81340477951
Mark Bruderer <markbruderer123@gmail.com>
2024-10-05 18:18:34 +0200

    fix: sequentially ordered elements.

--- components/directory/Item.vue
+++ components/directory/Item.vue
@@ -6,9 +6,9 @@ const props = defineProps(['item']);
   <NuxtLink :to="item._path"
     class="border border-dashed border-gray-200 dark:border-gray-500 hover:border-primary-400 hover:border-solid dark:hover:border-primary-300 rounded p-6 relative group transition-all">
     <DirectoryFeaturedTag v-if="item.featured" />
-    <h3 class="m-0 text-lg font-semibold dark:text-gray-50">
+    <h2 class="m-0 text-lg font-semibold dark:text-gray-50">
       {{ item.title }}
-    </h3>
+    </h2>
     <p>
       {{ item.description }}
     </p>
